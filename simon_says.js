8866226e5182598eea745f829803bfda217f7d52
cindychea <cindy_chea@hotmail.com>
2019-07-04 12:15:12 -0400

    completed reinforcement testing exercise

--- simon_says.js
+++ simon_says.js
@@ -1,1 +1,21 @@
+const echo = (S) => {
+  return S;
+}
+
+const shout = (S) => {
+  return S.toUpperCase();
+}
+
+const repeat = (S, N) => {
+  return (S + ' ').repeat(N).trim();
+}
+
+const startOfWord = (S, N) => {
+  return S.substring(0, N);
+}
+
+const firstWord = (S) => {
+  return S.split(' ')[0];
+}
+
 module.exports = {echo: echo, shout: shout, repeat: repeat, startOfWord: startOfWord, firstWord: firstWord};
